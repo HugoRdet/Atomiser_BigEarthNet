45dcabe3f5e7cac9650027c065ed245040e7e1b2
Hugo Riffaud de Turckheim <54743554+HugoRdet@users.noreply.github.com>
2025-04-24 09:08:35 +0200

    Initial commit

---
+++ script_evaluation.py
@@ -0,0 +1,139 @@
+from training.perceiver import *
+from training.utils import *
+from training.losses import *
+from training.VIT import *
+from training.ResNet import *
+from collections import defaultdict
+from training import *
+import os
+from pytorch_lightning import Trainer,seed_everything
+from pytorch_lightning.loggers import WandbLogger
+
+import torch
+import numpy as np
+from torch import nn, einsum
+import torch.nn.functional as F
+import einops as einops
+from einops import rearrange, repeat
+from einops.layers.torch import Reduce
+
+import matplotlib.pyplot as plt
+
+from configilm import util
+util.MESSAGE_LEVEL = util.MessageLevel.INFO  # use INFO to see all messages
+seed_everything(42, workers=True)
+from configilm.extra.DataSets import BENv2_DataSet
+from configilm.extra.DataModules import BENv2_DataModule
+import random
+
+
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Evaluation script")
+
+
+
+
+# Add the --run_id argument
+parser.add_argument("--run_id", type=str, required=True, help="WandB run id from training")
+
+# Add the --run_id argument
+parser.add_argument("--xp_name", type=str, required=True, help="Experiment name")
+
+# Add the --run_id argument
+parser.add_argument("--config_model", type=str, required=True, help="Model config yaml file")
+
+# Add the --run_id argument
+parser.add_argument("--dataset_name", type=str, required=True, help="Name of the dataset used")
+
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Access the run id
+run_id = args.run_id
+xp_name=args.xp_name
+config_model = args.config_model
+config_name_dataset = args.dataset_name
+
+print("Using WandB Run ID:", run_id)
+
+
+
+seed_everything(42, workers=True)
+
+torch.set_default_dtype(torch.float32)
+torch.set_float32_matmul_precision('medium')
+
+config_model = read_yaml("./training/configs/config_test-Atomiser_Atos.yaml")
+labels = load_json_to_dict("./data/labels.json")
+bands_yaml = "./data/bands_info/bands.yaml"
+
+trans_config = transformations_config_flair(bands_yaml, config_model)
+
+
+ 
+wand = True
+wandb_logger = None
+if wand:
+    if os.environ.get("LOCAL_RANK", "0") == "0":
+        import wandb
+        wandb.init(
+            id=run_id,            # Pass the run ID from the training run
+            resume='allow',       # Allow resuming the existing run
+            name=config_model['encoder'],
+            project="PLANTED",
+            config=config_model
+        )
+        wandb_logger = WandbLogger(project="PLANTED")
+
+
+checkpoint_dir = "./checkpoints"
+all_ckpt_files = [
+    os.path.join(checkpoint_dir, f) 
+    for f in os.listdir(checkpoint_dir) 
+    if f.endswith(".ckpt")
+]
+
+# Try to filter files by xp_name if any checkpoint includes it.
+checkpoint_files = [f for f in all_ckpt_files if xp_name in os.path.basename(f)]
+
+# If no file contains xp_name, fallback to using all checkpoint files.
+if not checkpoint_files:
+    checkpoint_files = all_ckpt_files
+
+if not checkpoint_files:
+    raise FileNotFoundError(f"No checkpoint found in {checkpoint_dir}.")
+
+# Sort the selected checkpoint files by modification time and choose the most recent one.
+checkpoint_path = sorted(checkpoint_files, key=os.path.getmtime)[-1]
+print(f"Loading best checkpoint: {checkpoint_path}")
+
+model = Model.load_from_checkpoint(checkpoint_path, config=config_model, wand=wand, name=xp_name, labels=labels,transform=trans_config)
+
+
+data_module = CustomPlantedDataModule(
+    "./data/custom_planted/"+config_name_dataset,
+    config=config_model,
+    trans_config=trans_config,
+    batch_size=config_model['dataset']['batchsize'],
+)
+
+
+
+
+# Trainer
+test_trainer = Trainer(
+    use_distributed_sampler=False,
+    max_epochs=config_model["trainer"]["epochs"],
+    logger=wandb_logger,
+    log_every_n_steps=1,
+    accelerator="gpu",
+    default_root_dir="./checkpoints/"
+)
+
+# Evaluate the model
+test_trainer.test(model, datamodule=data_module)
+
+
---
+++ training/trainer.py
@@ -0,0 +1,351 @@
+from training.perceiver import *
+from training.atomiser import *
+from training.utils import *
+from training.losses import *
+from training.VIT import *
+from training.ResNet import *
+from collections import defaultdict
+from training import *
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import WandbLogger
+import torch
+import numpy as np
+from torch import nn, einsum
+import torch.nn.functional as F
+import einops as einops
+from einops import rearrange, repeat
+from einops.layers.torch import Reduce
+import matplotlib.pyplot as plt
+import random
+import torchmetrics
+import warnings
+import wandb
+from torch.distributed import broadcast
+
+#BigEarthNet...
+warnings.filterwarnings("ignore", message="No positive samples found in target, recall is undefined. Setting recall to one for all thresholds.")
+
+class Model(pl.LightningModule):
+    def __init__(self, config,labels, wand, name,transform=None):
+        super().__init__()
+        self.config = config
+        self.labels=labels
+        self.wand = wand
+        self.transform=transform
+        self.num_classes = config["trainer"]["num_classes"]
+        self.logging_step = config["trainer"]["logging_step"]
+        self.actual_epoch = 0
+        weights_loss= torch.sqrt(1+self.get_label_weights().to(torch.float32))
+
+        # Ensure same weights on all GPUs
+        if torch.distributed.is_initialized():
+            torch.distributed.broadcast(weights_loss, src=0)
+
+        self.weights_loss=weights_loss
+
+        self.best_val_loss = float("inf")
+        self.best_val_ap = float("-inf")
+        self.best_train_loss = float("inf")
+        self.best_train_ap = float("-inf")
+        self.labels_idx = labels
+        self.weight_decay = float(config["trainer"]["weight_decay"])
+        self.mode = "training"
+        self.multi_modal = config["trainer"]["multi_modal"]
+        self.name = name
+        self.table=False
+
+        self.metric_Acc_train = torchmetrics.Accuracy(task="multiclass", num_classes=40,average="micro")
+        self.metric_Acc_validation = torchmetrics.Accuracy(task="multiclass", num_classes=40,average="micro")
+        self.metric_Acc_test = torchmetrics.Accuracy(task="multiclass", num_classes=40,average="micro")
+
+        self.metric_F1_train_per_class = torchmetrics.classification.MulticlassF1Score(num_classes=40, average="none")
+        self.metric_F1_val_per_class = torchmetrics.classification.MulticlassF1Score(num_classes=40, average="none")
+        self.metric_F1_test_per_class = torchmetrics.classification.MulticlassF1Score(num_classes=40, average="none")
+
+        
+
+
+        self.confmat_test = torchmetrics.ConfusionMatrix(
+            task='multiclass',
+            num_classes=self.num_classes
+        )
+
+
+        if config["encoder"] == "Atomiser":
+            self.encoder = Atomiser(
+                config=self.config,
+                transform=self.transform,
+                depth=config["Atomiser"]["depth"],
+                num_latents=config["Atomiser"]["num_latents"],
+                latent_dim=config["Atomiser"]["latent_dim"],
+                cross_heads=config["Atomiser"]["cross_heads"],
+                latent_heads=config["Atomiser"]["latent_heads"],
+                cross_dim_head=config["Atomiser"]["cross_dim_head"],
+                latent_dim_head=config["Atomiser"]["latent_dim_head"],
+                num_classes=config["trainer"]["num_classes"],
+                attn_dropout=config["Atomiser"]["attn_dropout"],
+                ff_dropout=config["Atomiser"]["ff_dropout"],
+                weight_tie_layers=config["Atomiser"]["weight_tie_layers"],
+                self_per_cross_attn=config["Atomiser"]["self_per_cross_attn"],
+                final_classifier_head=config["Atomiser"]["final_classifier_head"],
+                masking=config["Atomiser"]["masking"]
+            )
+
+
+        self.common_classes,self.frequent_classes,self.rare_classes=self.get_label_frequencies()
+
+      
+        
+
+
+  
+        self.loss = nn.CrossEntropyLoss(weight=self.weights_loss)
+        self.lr = float(config["trainer"]["lr"])
+        self.max_tokens=self.config["trainer"]["max_tokens"]
+
+    def get_label_frequencies(self):
+     
+
+        ids_common=[]
+        ids_rare=[]
+        ids_frequent=[]
+
+        for label in self.labels:
+           
+            if self.labels[label]["Frequency"]=="frequent":
+                ids_frequent.append(int(self.labels[label]["id"]))
+            
+            if self.labels[label]["Frequency"]=="common":
+                ids_common.append(int(self.labels[label]["id"]))
+
+            if self.labels[label]["Frequency"]=="rare":
+                ids_rare.append(int(self.labels[label]["id"]))
+
+        ids_common=torch.from_numpy(np.array(ids_common))
+        ids_rare=torch.from_numpy(np.array(ids_rare))
+        ids_frequent=torch.from_numpy(np.array(ids_frequent))
+        return ids_common,ids_frequent,ids_rare
+
+    def get_label_weights(self):
+        weights=dict()
+
+    
+
+        cpt_count=0
+        for label in self.labels:
+            weights[int(self.labels[label]["id"])]=int(self.labels[label]["count"])
+            cpt_count+=int(self.labels[label]["count"])
+
+
+
+        res=[]
+        for w in range(len(self.labels.keys())):
+            res.append(weights[w])
+        
+        res=torch.from_numpy(np.array(res)).to(float)
+        res*=40
+        res=cpt_count/res
+
+        return res
+        
+    def forward(self, tokens,training=False):
+        model_output=self.encoder(tokens,training=training)
+       
+     
+      
+        return model_output
+    
+
+            
+
+
+    def training_step(self, batch, batch_idx):
+        
+
+        labels,frequency=batch[-2:]
+        y_hat = self.forward(batch[:-2],training=False)
+        
+        labels=labels.to(torch.long)
+        loss = self.loss(y_hat, labels)
+
+        y_hat=torch.argmax(y_hat,dim=1)
+        
+
+        self.metric_Acc_train.update(y_hat,labels)
+        self.metric_F1_train_per_class.update(y_hat, labels)
+
+
+        self.log("train_loss", loss, on_step=True, on_epoch=False, logger=True, sync_dist=False)
+        self.log("log train_loss", torch.log(loss), on_step=True, on_epoch=False, logger=True, sync_dist=False)
+
+        
+
+        return loss 
+
+    
+
+        
+    def on_train_epoch_end(self):
+        metrics = self.trainer.callback_metrics
+        loss = metrics.get("train_loss", float("inf"))
+
+        
+
+        Acc=self.metric_Acc_train.compute()
+        f1s = self.metric_F1_train_per_class.compute()
+        f1_freq = f1s[self.frequent_classes].mean()
+        f1_com = f1s[self.common_classes].mean()
+        f1_rare = f1s[self.rare_classes].mean()
+        
+        #self.log("train_loss", loss, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        
+
+        self.log("train_Acc", Acc, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("train_F1", f1s.mean(), on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("train_F1_freq", f1_freq, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("train_F1_com", f1_com, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("train_F1_rare", f1_rare, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+
+        self.metric_Acc_train.reset()
+        self.metric_F1_train_per_class.reset()
+
+    
+    
+
+
+
+ 
+
+
+    
+    
+    def validation_step(self, batch, batch_idx):
+        
+        labels,frequency=batch[-2:]
+        y_hat = self.forward(batch[:-2],training=False)
+
+
+       
+        
+        labels=labels.to(torch.long)
+        loss = self.loss(y_hat, labels)
+
+      
+
+        #if torch.isnan(loss) or torch.isinf(loss):
+        #    print(f"[Rank {self.global_rank}] Loss is NaN!")
+        #    print("y_hat:", y_hat)
+        #    print("labels:", labels)
+        #    print("weights_loss:", self.weights_loss)
+
+
+
+        y_hat=torch.argmax(y_hat,dim=1) #
+
+        
+        
+
+        self.metric_Acc_validation.update(y_hat,labels)
+        self.metric_F1_val_per_class.update(y_hat, labels)
+
+
+        self.log("val_loss", loss, on_step=False, on_epoch=True, logger=True, sync_dist=False)
+
+        return loss
+
+    def on_validation_epoch_end(self):
+        
+        metrics = self.trainer.callback_metrics
+        loss = metrics.get("val_loss", float("inf"))
+
+
+        Acc=self.metric_Acc_validation.compute()
+        f1s = self.metric_F1_val_per_class.compute()
+        f1_freq = f1s[self.frequent_classes].mean()
+        f1_com = f1s[self.common_classes].mean()
+        f1_rare = f1s[self.rare_classes].mean()
+        
+        self.log("val_loss", loss, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("log val_loss", torch.log(loss), on_step=False, on_epoch=True, logger=True, sync_dist=True)
+
+        self.log("val_Acc", Acc, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("val_F1", f1s.mean(), on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("val_F1_freq", f1_freq, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("val_F1_com", f1_com, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("val_F1_rare", f1_rare, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+
+        self.metric_Acc_validation.reset()
+        self.metric_F1_val_per_class.reset()
+        
+        
+            
+        
+    
+        
+    def on_test_epoch_start(self):
+        super().on_test_epoch_start()
+        
+    def test_step(self, batch, batch_idx):
+        labels,frequency=batch[-2:]
+        y_hat = self.forward(batch[:-2],training=False)
+        
+        labels=labels.to(torch.long)
+        
+
+        y_hat=torch.argmax(y_hat,dim=1)
+        
+
+        self.metric_Acc_test.update(y_hat,labels)
+        self.metric_F1_test_per_class.update(y_hat, labels)
+
+
+        
+
+        return None
+
+
+    def on_test_epoch_end(self):
+  
+
+        Acc=self.metric_Acc_test.compute()
+        f1s = self.metric_F1_test_per_class.compute()
+        f1_freq = f1s[self.frequent_classes].mean()
+        f1_com = f1s[self.common_classes].mean()
+        f1_rare = f1s[self.rare_classes].mean()
+        
+        
+
+        self.log("test_Acc", Acc, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("test_F1", f1s.mean(), on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("test_F1_freq", f1_freq, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("test_F1_com", f1_com, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+        self.log("test_F1_rare", f1_rare, on_step=False, on_epoch=True, logger=True, sync_dist=True)
+
+        self.metric_Acc_test.reset()
+        self.metric_F1_test_per_class.reset()
+
+
+        
+        
+        
+        
+    def save_model(self, name=None):
+        if name is not None:
+            file_path = f"./pth_files/{self.config['encoder']}_{self.name}_{name}.pth"
+        else:
+            file_path = f"./pth_files/{self.config['encoder']}_{self.name}.pth"
+        torch.save(self.encoder.state_dict(), file_path)
+        
+    def load_model(self, name=None):
+        if name is not None:
+            file_path = f"./pth_files/{self.config['encoder']}_{self.name}_{name}.pth"
+        else:
+            file_path = f"./pth_files/{self.config['encoder']}_{self.name}.pth"
+        self.encoder.load_state_dict(torch.load(file_path, weights_only=True))
+        
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        cosine_anneal_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.config["trainer"]["epochs"]*2, eta_min=0.0)#
+        #cosine_anneal_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=30000, T_mult=1, eta_min=0.0)
+        return {'optimizer': optimizer, 'lr_scheduler': {'scheduler': cosine_anneal_scheduler, 'interval': 'epoch', 'monitor': 'val_loss'}}
+
