e3375c30b4f5f971e574fa45f5c464c3237406cd
Hugo Riffaud de Turckheim <hugordet@gmail.com>
2025-04-29 16:55:35 +0200

    ok

--- training/perceiver/perceiver.py
+++ training/perceiver/perceiver.py
@@ -1,8 +1,5 @@
-from .utils import*
-from .nn_comp import*
-from .encoding import*
-
-
+from math import pi, log
+from functools import wraps
 
 import torch
 from torch import nn, einsum
@@ -11,8 +8,13 @@ import torch.nn.functional as F
 from einops import rearrange, repeat
 from einops.layers.torch import Reduce
 
+# helpers
 
+def exists(val):
+    return val is not None
 
+def default(val, d):
+    return val if exists(val) else d
 
 def cache_fn(f):
     cache = dict()
@@ -28,6 +30,97 @@ def cache_fn(f):
         return result
     return cached_fn
 
+def fourier_encode(x, max_freq, num_bands = 4):
+    x = x.unsqueeze(-1)
+    device, dtype, orig_x = x.device, x.dtype, x
+
+    scales = torch.linspace(1., max_freq / 2, num_bands, device = device, dtype = dtype)
+    scales = scales[(*((None,) * (len(x.shape) - 1)), Ellipsis)]
+
+    x = x * scales * pi
+    x = torch.cat([x.sin(), x.cos()], dim = -1)
+    x = torch.cat((x, orig_x), dim = -1)
+    return x
+
+# helper classes
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn, context_dim = None):
+        super().__init__()
+        self.fn = fn
+        self.norm = nn.LayerNorm(dim)
+        self.norm_context = nn.LayerNorm(context_dim) if exists(context_dim) else None
+
+    def forward(self, x, **kwargs):
+        x = self.norm(x)
+
+        if exists(self.norm_context):
+            context = kwargs['context']
+            normed_context = self.norm_context(context)
+            kwargs.update(context = normed_context)
+
+        return self.fn(x, **kwargs)
+
+class GEGLU(nn.Module):
+    def forward(self, x):
+        x, gates = x.chunk(2, dim = -1)
+        return x * F.gelu(gates)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult = 4, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, dim * mult * 2),
+            GEGLU(),
+            nn.Linear(dim * mult, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, query_dim, context_dim = None, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head * heads
+        context_dim = default(context_dim, query_dim)
+
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+
+        self.to_q = nn.Linear(query_dim, inner_dim, bias = False)
+        self.to_kv = nn.Linear(context_dim, inner_dim * 2, bias = False)
+
+        self.dropout = nn.Dropout(dropout)
+        self.to_out = nn.Linear(inner_dim, query_dim)
+
+    def forward(self, x, context = None, mask = None):
+        h = self.heads
+
+        q = self.to_q(x)
+        context = default(context, x)
+        k, v = self.to_kv(context).chunk(2, dim = -1)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h = h), (q, k, v))
+
+        sim = einsum('b i d, b j d -> b i j', q, k) * self.scale
+
+        if exists(mask):
+            mask = rearrange(mask, 'b ... -> b (...)')
+            max_neg_value = -torch.finfo(sim.dtype).max
+            mask = repeat(mask, 'b j -> (b h) () j', h = h)
+            sim.masked_fill_(~mask, max_neg_value)
+
+        # attention, what we cannot get enough of
+        attn = sim.softmax(dim = -1)
+        attn = self.dropout(attn)
+
+        out = einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, '(b h) n d -> b n (h d)', h = h)
+        return self.to_out(out)
+
+# main class
+
 class Perceiver(nn.Module):
     def __init__(
         self,
@@ -35,7 +128,7 @@ class Perceiver(nn.Module):
         num_freq_bands,
         depth,
         max_freq,
-        input_channels = 3,
+        input_channels = 12,
         input_axis = 2,
         num_latents = 512,
         latent_dim = 512,
@@ -43,7 +136,7 @@ class Perceiver(nn.Module):
         latent_heads = 8,
         cross_dim_head = 64,
         latent_dim_head = 64,
-        num_classes = 1000,
+        num_classes = 19,
         attn_dropout = 0.,
         ff_dropout = 0.,
         weight_tie_layers = False,
@@ -85,49 +178,19 @@ class Perceiver(nn.Module):
         self.num_freq_bands = num_freq_bands
 
         self.fourier_encode_data = fourier_encode_data
-        fourier_channels = (input_axis * ((self.num_freq_bands * 2) + 1)) if fourier_encode_data else 0
+        fourier_channels = (input_axis * ((num_freq_bands * 2) + 1)) if fourier_encode_data else 0
         input_dim = fourier_channels + input_channels
 
-        #https://pytorch.org/docs/stable/generated/torch.nn.parameter.Parameter.html
         self.latents = nn.Parameter(torch.randn(num_latents, latent_dim))
 
-        get_cross_attn = cache_fn(lambda: PreNorm(
-            latent_dim,
-            CrossAttention(
-                query_dim   = latent_dim,
-                context_dim = input_dim,
-                heads       = cross_heads,
-                dim_head    = cross_dim_head,
-                dropout     = attn_dropout,
-                use_flash   = True
-            ),
-            context_dim = input_dim
-        ))
+        get_cross_attn = lambda: PreNorm(latent_dim, Attention(latent_dim, input_dim, heads = cross_heads, dim_head = cross_dim_head, dropout = attn_dropout), context_dim = input_dim)
+        get_cross_ff = lambda: PreNorm(latent_dim, FeedForward(latent_dim, dropout = ff_dropout))
+        get_latent_attn = lambda: PreNorm(latent_dim, Attention(latent_dim, heads = latent_heads, dim_head = latent_dim_head, dropout = attn_dropout))
+        get_latent_ff = lambda: PreNorm(latent_dim, FeedForward(latent_dim, dropout = ff_dropout))
 
-        get_cross_ff = cache_fn(lambda: PreNorm(
-            latent_dim,
-            FeedForward(latent_dim, dropout=ff_dropout)
-        ))
-
-        get_latent_attn = cache_fn(lambda: PreNorm(
-            latent_dim,
-            SelfAttention(
-                dim        = latent_dim,
-                heads      = latent_heads,
-                dim_head   = latent_dim_head,
-                dropout    = attn_dropout,
-                use_flash  = True
-            )
-        ))
-
-        get_latent_ff = cache_fn(lambda: PreNorm(
-            latent_dim,
-            FeedForward(latent_dim, dropout=ff_dropout)
-        ))
         get_cross_attn, get_cross_ff, get_latent_attn, get_latent_ff = map(cache_fn, (get_cross_attn, get_cross_ff, get_latent_attn, get_latent_ff))
 
         self.layers = nn.ModuleList([])
-        #weights are shared except for the first blocs of cross attention / latent attention
         for i in range(depth):
             should_cache = i > 0 and weight_tie_layers
             cache_args = {'_cache': should_cache}
@@ -152,9 +215,12 @@ class Perceiver(nn.Module):
             nn.Linear(latent_dim, num_classes)
         ) if final_classifier_head else nn.Identity()
 
-
-    def forward(self,data,mask = None,return_embeddings = False):
-        data = rearrange(data, 'b c h w-> b h w c')
+    def forward(
+        self,
+        data,
+        mask = None,
+        return_embeddings = False
+    ):
         b, *axis, _, device, dtype = *data.shape, data.device, data.dtype
         assert len(axis) == self.input_axis, 'input data must have the right number of axis'
 
@@ -169,8 +235,8 @@ class Perceiver(nn.Module):
 
             data = torch.cat((data, enc_pos), dim = -1)
 
-     
         # concat to channels of data and flatten axis
+
         data = rearrange(data, 'b ... d -> b (...) d')
 
         x = repeat(self.latents, 'n d -> b n d', b = b)
