3c44234a263456238e06d446ea4d52b7a41bcd6d
Hugo Riffaud de Turckheim <hugordet@gmail.com>
2025-04-30 01:49:03 +0200

    ok

--- script_evaluation.py
+++ script_evaluation.py
@@ -80,7 +80,7 @@ wandb_logger = None
 if wand:
     if os.environ.get("LOCAL_RANK", "0") == "0":
         import wandb
-        wandb.init(
+        run=wandb.init(
             #id=run_id,            # Pass the run ID from the training run
             #resume='allow',       # Allow resuming the existing run
             name=config_model['encoder'],
